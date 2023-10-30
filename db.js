f41e1912e0a62619213e2922d556fcb481cced0b
Katharina  Silveira <125597346+Katharinasilveira@users.noreply.github.com>
2023-10-30 14:15:26 -0300

    Add files via upload

---
+++ db.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql')
+
+const connection = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"rocket"
+})
+    connection.connect()
+
+    //inserir
+    function inserir(){
+    let sql = "insert into foguete set ?"
+    let dados = {nomefoguete:"MyRocket",tipo:"protótipo",ano:2023}
+    connection.query(sql, dados, function(error, results, fields){
+        if(error) throw error
+        console.log("Cadastrado!")
+})
+connection.end()
+}
+
+inserir()
